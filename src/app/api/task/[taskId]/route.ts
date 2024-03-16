bfbb231e50b6a1bea948fb4a601b5574dc77d498
Abhishek Dagar <dagarabhishek584@gmail.com>
2024-03-16 19:17:12 +0530

    remove bug

--- src/app/api/task/[taskId]/route.ts
+++ src/app/api/task/[taskId]/route.ts
@@ -8,5 +8,9 @@ export const GET = async (req: NextRequest, context: { params: "string" }) => {
   try {
     const { taskId }: any = context.params;
     const tasks = await Task.find({});
+    return NextResponse.json(
+      { message: "project fetched", data: {}, success: true },
+      { status: 200 }
+    );
   } catch {}
 };
