ae034adf9136719dd1c5d87411be64efd2d4f9f7
Abhishek Dagar <dagarabhishek584@gmail.com>
2024-06-01 16:24:39 +0530

    disabling the suspense rule

--- src/app/(main)/app/teams/page.tsx
+++ src/app/(main)/app/teams/page.tsx
@@ -11,7 +11,7 @@ const Teams = async () => {
   return (
     <div
       className={`flex flex-col gap-4 relative ${
-        teams.length > 0 ? "" : "h-full"
+        teams?.length > 0 ? "" : "h-full"
       }`}
     >
       <div className="sticky top-0 z-[10] py-3 px-6 bg-background/50 backdrop-blur-lg flex items-center justify-between border-b">
@@ -22,9 +22,9 @@ const Teams = async () => {
         {/* <Button variant={"secondary"}>Create Team</Button> */}
         <CreateNewTeamModal />
       </div>
-      {teams.length > 0 ? (
+      {teams?.length > 0 ? (
         <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 px-10">
-          {teams.map((team: any) => (
+          {teams?.map((team: any) => (
             <Link key={team.id} href={`/app/teams/${team.id}`}>
               <TeamCard team={team} />
             </Link>
